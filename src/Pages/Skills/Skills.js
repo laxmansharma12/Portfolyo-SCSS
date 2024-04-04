446c1ddefe1bdab9ffc522e94351bc20642cad48
laxman <laxmansharmawe@gmail.com>
2024-04-05 00:08:59 +0530

    changed list style of skills

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -17,57 +17,25 @@ const Skills = () => {
 			</div>
 			<div className="SkillsInnerConatiner">
 				{sortedSkills &&
-					sortedSkills.map(
-						(skill, index) =>
-							// Render two items per iteration
-							index % 2 === 0 && (
-								<div className="skillRow" key={index}>
-									<div className="skillItem1">
-										<div className="item1">
-											<img src={skill?.image?.url} alt="" />
-											<div className="skillDetails">
-												<p className="skillName">{skill?.name}</p>
-												<div className="percentage">
-													<div
-														role="progressbar"
-														aria-valuenow={sortedSkills[index + 1].percentage}
-														aria-valuemin="0"
-														aria-valuemax="100"
-														style={{
-															"--value": sortedSkills[index + 1].percentage,
-														}}
-													></div>
-												</div>
-											</div>
-										</div>
-									</div>
-									{/* Check if next item exists before rendering */}
-									{sortedSkills[index + 1] && (
-										<div className="skillItem2" key={index + 1}>
-											<div className="item2">
-												<div className="skillDetails">
-													<p className="skillName">
-														{sortedSkills[index + 1]?.name}
-													</p>
-													<div className="percentage">
-														<div
-															role="progressbar"
-															aria-valuenow={sortedSkills[index + 1].percentage}
-															aria-valuemin="0"
-															aria-valuemax="100"
-															style={{
-																"--value": sortedSkills[index + 1].percentage,
-															}}
-														></div>
-													</div>
-												</div>
-												<img src={sortedSkills[index + 1]?.image?.url} alt="" />
-											</div>
-										</div>
-									)}
+					sortedSkills.map((skill, index) => (
+						<div className="item" key={index}>
+							<img src={skill?.image?.url} alt="" />
+							<div className="skillDetails">
+								<p className="skillName">{skill?.name}</p>
+								<div className="percentage">
+									<div
+										role="progressbar"
+										aria-valuenow={skill.percentage}
+										aria-valuemin="0"
+										aria-valuemax="100"
+										style={{
+											"--value": skill.percentage,
+										}}
+									></div>
 								</div>
-							)
-					)}
+							</div>
+						</div>
+					))}
 			</div>
 		</div>
 	);
